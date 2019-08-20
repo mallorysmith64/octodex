48a409a37d6a136a2244e270f9dc6db181369c4d
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-19 22:03:24 -0400

    added styling to background and photos

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,16 @@ class Nav extends Component {
   render() {
     return (
       <div>
-        <nav className="main-menu">
-          <h2>Octodex</h2>
-          <h1>Home</h1>
-          <h1>FAQ</h1>
-          <h1>Follow us on Twitter</h1>
-          <h1>Back to GitHub.com</h1>
+        <nav>
+          <section class="main-menu-left">
+            <h2>Octodex</h2>
+            <h1>Home</h1>
+            <h1>FAQ</h1>
+          </section>
+          <section class="main-menu-right">
+            <h1>Follow us on Twitter</h1>
+            <h1>Back to GitHub.com</h1>
+          </section>
         </nav>
       </div>
     )

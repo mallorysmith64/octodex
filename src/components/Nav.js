3260281c6cbbc368ee5dea2674635bd32e7dc50e
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-19 22:54:55 -0400

    styled nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,19 +3,17 @@ import React, { Component } from 'react'
 class Nav extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          <section class="main-menu-left">
-            <h2>Octodex</h2>
-            <h1>Home</h1>
-            <h1>FAQ</h1>
-          </section>
-          <section class="main-menu-right">
-            <h1>Follow us on Twitter</h1>
-            <h1>Back to GitHub.com</h1>
-          </section>
-        </nav>
-      </div>
+      <nav>
+        <section class="main-menu-left">
+          <h2>Octodex</h2>
+          <h1>Home</h1>
+          <h1>FAQ</h1>
+        </section>
+        <section class="main-menu-right">
+          <h1>Follow us on Twitter</h1>
+          <h1>Back to GitHub.com</h1>
+        </section>
+      </nav>
     )
   }
 }

05f0228995c36d2d5b8d67c26fcc4b2fff3e4265
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-19 20:24:10 -0400

    starting to style

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -4,7 +4,7 @@ class Cat extends Component {
   render() {
     return (
       <li>
-        <img src={this.props.imageUrl} alt={this.props.imageAlt} height={400} />
+        <img src={this.props.imageUrl} alt={this.props.imageAlt} height={600} />
         <h2>{this.props.number}</h2>
         <h2>{this.props.name}</h2>
       </li>
